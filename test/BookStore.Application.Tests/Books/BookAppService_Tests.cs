cd5bc4070dc3ad6da2769b580424149a731ad1cb
seyitalisoy <soyseyitali23@gmail.com>
2025-10-23 12:47:59 +0300

    integration tests created

---
+++ test/BookStore.Application.Tests/Books/BookAppService_Tests.cs
@@ -0,0 +1,74 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Modularity;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace BookStore.Books
+{
+    public abstract class BookAppService_Tests<TStartupModule> : 
+        BookStoreApplicationTestBase<TStartupModule> where TStartupModule : IAbpModule
+    {
+        private readonly IBookAppService _bookAppService;
+
+        protected BookAppService_Tests()
+        {
+            _bookAppService = GetRequiredService<IBookAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_List_Of_Books()
+        {
+            var result = await _bookAppService.GetListAsync(
+                new PagedAndSortedResultRequestDto()
+            );
+
+            //Assert
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.Items.ShouldContain(b => b.Name == "1984");
+        }
+
+        [Fact]
+        public async Task Should_Not_Create_A_Book_Without_Name()
+        {
+            var exception = await Assert.ThrowsAsync<AbpValidationException>(async () =>
+            {
+                await _bookAppService.CreateAsync(
+                    new CreateUpdateBookDto
+                    {
+                        Name = "",
+                        Price = 10,
+                        PublishDate = DateTime.Now,
+                        Type = BookType.ScienceFiction
+                    }
+                    );
+            });
+
+            exception.ValidationErrors
+                .ShouldContain(err => err.MemberNames.Any(mem => mem == "Name"));
+        }
+
+        [Fact]
+        public async Task Should_Create_A_Valid_Book()
+        {
+            //act
+            var result = await _bookAppService.CreateAsync(
+                new CreateUpdateBookDto
+                {
+                    Name = "This is test Book",
+                    Price = 10,
+                    PublishDate = DateTime.Now,
+                    Type = BookType.ScienceFiction
+                });
+            //assert
+            result.Id.ShouldNotBe(Guid.Empty);
+            result.Name.ShouldBe("This is test Book");
+
+        }
+    }
+}
---
+++ test/BookStore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/EfCoreBookAppService_Tests.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Books;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BookStore.EntityFrameworkCore.Applications.Books
+{
+    [Collection(BookStoreTestConsts.CollectionDefinitionName)]
+    public class EfCoreBookAppService_Tests: BookAppService_Tests<BookStoreEntityFrameworkCoreTestModule>
+    {
+    }
+}

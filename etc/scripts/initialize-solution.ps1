0e4a8e6dce982c91260f98f32019e5941b323d0d
seyitalisoy <soyseyitali23@gmail.com>
2025-10-23 09:48:24 +0300

    abp framework created

---
+++ etc/scripts/initialize-solution.ps1
@@ -0,0 +1,10 @@
+abp install-libs
+
+cd src/BookStore.DbMigrator && dotnet run && cd -
+
+
+cd src/BookStore.HttpApi.Host && dotnet dev-certs https -v -ep openiddict.pfx -p config.auth_server_default_pass_phrase 
+
+
+
+exit 0

a1535751906ccd6edd1d0117a265cd0d85d6d976
seyitalisoy <soyseyitali23@gmail.com>
2025-10-23 10:52:31 +0300

    abp proxy generated and bookComponent created

---
+++ angular/src/app/components/book/book.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  imports: [],
+  templateUrl: './book.html',
+  styleUrl: './book.scss'
+})
+export class BookComponent {
+
+}

a031ec7921f2ced1c1a740094bc16cbb120badf1
seyitalisoy <soyseyitali23@gmail.com>
2025-10-23 14:18:15 +0300

    permissions of books and their localizations created

--- src/BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ src/BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -1,10 +1,22 @@
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
 namespace BookStore.Permissions;
 
 public static class BookStorePermissions
 {
     public const string GroupName = "BookStore";
+    // other permissions...
+    // other permissions...
 
+    // *** ADDED a NEW NESTED CLASS ***
+    public static class Books
+    {
+        public const string Default = GroupName + ".Books";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
 
+    }
     
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";

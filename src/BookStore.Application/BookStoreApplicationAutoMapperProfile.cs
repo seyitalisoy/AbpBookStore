a51b66a0e04e0c03d8dfb43da11f71742ef5fcf7
seyitalisoy <soyseyitali23@gmail.com>
2025-10-24 13:47:24 +0300

    Author concrete service created

--- src/BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ src/BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using BookStore.Authors;
 using BookStore.Books;
 
 namespace BookStore;
@@ -9,5 +10,7 @@ public class BookStoreApplicationAutoMapperProfile : Profile
     {
         CreateMap<Book, BookDto>();
         CreateMap<CreateUpdateBookDto, Book>();
+
+        CreateMap<Author, AuthorDto>();
     }
 }

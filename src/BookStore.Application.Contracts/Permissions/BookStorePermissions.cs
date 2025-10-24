5942600f32e6b32815c2ad4a96339c1dd715b682
seyitalisoy <soyseyitali23@gmail.com>
2025-10-24 13:48:22 +0300

    BookStore permissions modified for Author operations

--- src/BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ src/BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -17,7 +17,15 @@ public static class BookStorePermissions
         public const string Delete = Default + ".Delete";
 
     }
-    
+
+    public static class Authors
+    {
+        public const string Default = GroupName + ".Authors";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
+
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
 }

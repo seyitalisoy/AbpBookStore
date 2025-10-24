9bb7fd1d44e89c17ecdf360fbbd91c95e7b1fa8d
seyitalisoy <soyseyitali23@gmail.com>
2025-10-24 11:04:03 +0300

    Author entity and relational classes created

---
+++ src/BookStore.Domain/Authors/Author.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Emailing;
+
+namespace BookStore.Authors
+{
+    public class Author : FullAuditedAggregateRoot<Guid>
+    {
+        public string Name { get; private set; }
+        public DateTime BirthDate { get; set; }
+        public string ShortBio { get; set; }
+
+        private Author()
+        {
+            
+        }
+
+        internal Author( Guid id, string name, DateTime birthdate, string? shortBio = null):
+            base(id)
+        {
+            SetName(name);
+            BirthDate = birthdate;
+            ShortBio = shortBio;
+        }
+
+        internal Author ChangeName(string name)
+        {
+            SetName(name);
+            return this;
+        }
+
+        private void SetName(string name)
+        {
+            Name = Check.NotNullOrWhiteSpace(
+                name,
+                nameof(name),
+                maxLength: AuthorConsts.MaxNameLength
+                );
+        }
+    }
+}

a58160c8ae7a3f08058cee0d406dc41107cbf13c
seyitalisoy <soyseyitali23@gmail.com>
2025-10-24 13:49:19 +0300

    integrated tests of authors

---
+++ test/BookStore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Authors/EfCoreAuthorAppService_Tests.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Authors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BookStore.EntityFrameworkCore.Applications.Authors
+{
+    [Collection(BookStoreTestConsts.CollectionDefinitionName)]
+    public class EfCoreAuthorAppService_Tests : AuthorAppService_Tests<BookStoreEntityFrameworkCoreTestModule>
+    {
+
+    }
+}
